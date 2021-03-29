298cd97f181a97c1ac79eb21546782ac2374e13b
Janneth Verduzco <janneth_verduzco@live.com>
2021-03-28 23:13:01 -0500

    completed index.js to export router

--- routes/api/book.js
+++ routes/api/book.js
@@ -3,12 +3,12 @@ const router = require("express").Router();
 const bookController = require("../../controllers/bookController")
 
 // Setting books route to get and create
-router.route("/")
+router.route("/api/books")
 .get(bookController.findAll)
 .post(bookController.create);
 
 // Setting specifi book route to get, update, or delete the book
-router.route("/:id").get(bookController.findById)
+router.route("/api/books/:id").get(bookController.findById)
 .put(bookController.update)
 .delete(bookController.remove)
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 // Dependecies
 const path = require("path");
+const { route } = require("./api");
 // Allowing express to use this router
 const router = require("express").Router();
 const apiRoutes = require("./api")
+
+//routes
+router.use("/api", apiRoutes);
+
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
